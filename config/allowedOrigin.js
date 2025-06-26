b26ecbf3af834e7687024932a9507b7219ca18d0
Hammam Hussein <hamamhussein10@gmail.com>
2025-06-26 00:04:19 -0700

    allow origin

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,5 +1,5 @@
 export const allowedOrigins = [
   'http://localhost:5173',
   'http://127.0.0.1:5173',
-  'https://upload-pdf-uni-frontend-p9stlwcj6-hammam-husseins-projects.vercel.app',
+  'https://pdf-project-uni.vercel.app',
 ];
