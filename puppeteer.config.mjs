1df3c9c27a32024bbbb31adb2703f8dbac4fd666
Hammam Hussein <hamamhussein10@gmail.com>
2025-06-27 02:33:52 -0700

    Puppeteer Config to mjs

--- puppeteer.config.mjs
+++ puppeteer.config.mjs
@@ -1,5 +1,10 @@
 import { join } from 'path';
+import { cwd } from 'process';
 /**
  * @type {import('puppeteer').Configuration}
  */
 module.exports = { cachedDirectory: join(__dirname, '.cache', 'puppeteer') };
+
+export default {
+  cachedDirectory: join(cwd(), '.cache', 'puppeteer'),
+};
