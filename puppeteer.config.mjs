80278a5ad4656830654cb472d651a0abdd695a2c
Hammam Hussein <hamamhussein10@gmail.com>
2025-06-27 03:01:00 -0700

    puppeteer mjs

--- puppeteer.config.mjs
+++ puppeteer.config.mjs
@@ -5,5 +5,5 @@ import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default {
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+  cacheDir: join(__dirname, '.cache', 'puppeteer'),
 };
